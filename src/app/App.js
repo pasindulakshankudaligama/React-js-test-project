7481b03dbb2ce07ccfc9ed8467d28f11ae281910
pasindulakshankudaligama <kudaligamagepasindulakshan@gmail.com>
2022-07-19 12:42:21 +0530

    updated

--- src/app/App.js
+++ src/app/App.js
@@ -1,17 +1,20 @@
+
 import React from "react";
-import HomePage from "../pages/home"
+import HomePage from "../pages/home/index";
 import Login from "../pages/home/session/Login";
-import {Routes,Route} from "react-router-dom"
+import {Routes, Route} from "react-router-dom";
+import NotFound from "../pages/home/session/NotFoundIndex";
+
 
 function App() {
-  return (
+    return (
+        <Routes>
+            <Route exact path='/' element={<HomePage/>}/>
+            <Route path='login' element={<Login/>}/>
+            <Route path="*" element={<NotFound/>}/>
+        </Routes>
 
-      <Routes>
-        <Route exact='/' element={<HomePage/>}/>
-        <Route exact='login' element={<Login/>}/>
-        <Route exact="*" element={}/>
-      </Routes>
-  );
+    );
 }
 
 export default App;
