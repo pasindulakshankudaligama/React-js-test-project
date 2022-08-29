6188b1e6e54ffc3c7a9d824b969974a28801e11d
pasindulakshankudaligama <kudaligamagepasindulakshan@gmail.com>
2022-08-29 15:10:41 +0530

    dashboard js added

---
+++ src/pages/Dashboard/style.js
@@ -0,0 +1,38 @@
+export const styleSheet ={
+
+    container:{
+        display:'flex',
+        flexDirection:'column',
+        // backgroundColor:'red',
+        height:'100vh',
+        width:'100vw'
+    },
+
+    nav_div:{
+        display: 'flex',
+        alignItems:'center',
+        justifyContent:'space-around',
+        gap:'900px',
+        // backgroundColor: 'green',
+        height: '10%',
+        boxShadow:'0px 3px 20px -8px rgba(0,0,0,0.64)'
+    },
+
+    content_div:{
+        // backgroundColor: 'gray',
+        height: '90%',
+        display:'flex',
+        gap: '30px',
+        margin:'30px'
+    },
+
+    btn_container:{
+        display:'flex',
+        gap: '30px',
+    },
+
+    link:{
+        textDecoration:'none !important'
+    }
+
+}
