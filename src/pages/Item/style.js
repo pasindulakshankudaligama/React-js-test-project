d69d29f8e2d4fa0007d50767dcf261e6d5e1a24b
pasindulakshankudaligama <kudaligamagepasindulakshan@gmail.com>
2022-08-29 18:58:15 +0530

    item js added

---
+++ src/pages/Item/style.js
@@ -0,0 +1,57 @@
+export const styleSheet ={
+
+    container:{
+        display:'flex',
+        flexDirection:'column',
+        // backgroundColor:'red',
+        height:'100vh',
+        width:'100vw'
+    },
+
+    nav_div:{
+        display: 'flex',
+        alignItems:'center',
+        justifyContent:'space-around',
+        gap:'900px',
+        // backgroundColor: 'green',
+        height: '10%',
+        boxShadow:'0px 3px 20px -8px rgba(0,0,0,0.64)'
+    },
+
+    content_div:{
+        // backgroundColor: 'gray',
+        height: '90%',
+
+        margin:'30px'
+    },
+
+    btn_container:{
+        display:'flex',
+        gap: '30px',
+    },
+
+    link:{
+        textDecoration:'none !important'
+    },
+
+    form_div:{
+        display:'flex',
+        justifyContent:'center',
+        gap: '30px',
+    },
+
+    txt_div:{
+        display:'flex',
+        width: '50%',
+        gap: '30px',
+        flexDirection: 'column',
+    },
+
+    btn_div:{
+        display:'flex',
+        justifyContent:'flex-end',
+        marginTop:'30px',
+        marginBottom:'30px',
+        gap: '30px',
+    }
+}
