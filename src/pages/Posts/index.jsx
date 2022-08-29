c1af401d42a4577c468ea1e7c258950e3236f407
pasindulakshankudaligama <kudaligamagepasindulakshan@gmail.com>
2022-08-29 14:39:32 +0530

    post index updated

--- src/pages/Posts/index.jsx
+++ src/pages/Posts/index.jsx
@@ -1,154 +1,158 @@
-import React, {Component, Fragment} from "react";
-import {styleSheet} from "./style";
+import {Component} from "react";
 import {withStyles} from "@mui/styles";
-import Grid from '@mui/material/Grid';
-import {Typography} from "@mui/material";
-import GDSEButton from "../../components/common/Button";
-import {TextValidator, ValidatorForm} from 'react-material-ui-form-validator';
-import PostService from "../../services/PostService";
-import GDSESnackBar from "../../components/common/SnackBar";
-import Json from "qs";
+import {styleSheet} from "./style";
+import {
+    Autocomplete,
+    Card,
+    CardActions,
+    CardContent, Paper,
+    Table, TableBody, TableCell,
+    TableContainer, TableHead, TableRow,
+    TextField,
+    Typography
+} from "@mui/material";
+import Button from "@mui/material/Button";
+import {Link} from "react-router-dom";
+import { ValidatorForm, TextValidator} from 'react-material-ui-form-validator';
 
-class Posts extends Component {
+
+class Post extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            formData: {
-                userId: '',
-                id: '',
-                title: '',
-                body: ''
+            gender: [{label: 'Male'}, {label: 'Female'}],
+            post:{
+                userId:'',
+                title:'',
+                id:'',
+                body:''
             },
-            alert: false,
-            message: '',
-            severity: ''
+            customerTblData:[
+
+            ]
         }
     }
-    async loadData() {
-        let res = await PostService.fetchPosts()
-        if (res.status == 200) {
-            console.log("res: " + Json.stringify(res))
-        }else{
-            console.log("fetching error" +Json.stringify(res))
+
+    saveCustomer(){
+
+        /*let post = this.state.post;
+        let obj = {
+            userId:post.userId,
+            title:post.title,
+            id:post.id,
+            body:post.body
         }
-    }
-    componentDidMount() {
-        this.loadData();
-        console.log("Post screen mounted")
-    }
-    componentWillUnmount() {
-        console.log("Post screen unmounted")
-    }
 
-    handleSubmit = async () => {
-        console.log('save button clicked')
-        console.log(this.state.formData)
-        let formData = this.state.formData
-        let response = await PostService.createPost(formData);
-        if (response.status == 201) {
-            this.setState({
-                alert: true, message: 'Post created successfully', severity: 'success'
+        let {customerTblData} = this.state;
+        customerTblData.push(obj);
+        this.setState({customerTblData: customerTblData});*/
 
-            })
-        } else {
+        console.log("Click");
 
-            this.setState({
-                alert: true, message: 'Post created Unsuccessfully', severity: 'error'
-            })
-        }
     }
 
     render() {
+
         const {classes} = this.props;
-        return (
-            <Fragment>
-                <Typography variant="h4">
-                    Poster Manage
-                </Typography>
+
+        return (<div className={classes.container}>
+            <div className={classes.nav_div}>
+                <div>
+                    <Link className={classes.link} to={"/dashboard"}>
+                        <Typography variant="h4">
+                            Dashboard
+                        </Typography>
+                    </Link>
+                </div>
+                <div>
+                    <div className={classes.btn_container}>
+
+                        <Link className={classes.link} to={"/customer"}>
+                            <Button size="large">Customer</Button>
+                        </Link>
+
+                        <Link className={classes.link} to={"/item"}>
+                            <Button size="large">Item</Button>
+                        </Link>
+                    </div>
+                </div>
+            </div>
+            <div className={classes.content_div}>
                 <ValidatorForm
                     ref="form"
-                    onSubmit={this.handleSubmit}
+                    onSubmit={this.saveCustomer()}
+                    /*onSubmit={() => {
+                        this.saveCustomer()
+                        console.log("Click");
+                    }}*/
                     onError={errors => console.log(errors)}
                 >
-                    <Grid container spacing={0.5}>
-                        <Grid item lg={6} md={6} sm={6} xm={6}>
-                            <Typography variant="body2">User Id</Typography>
-                            <TextValidator
-                                id="outlined basic"
-                                placeholder="User Id"
-                                variant="outlined"
-                                size="small"
-                                value={this.state.formData.userId}
-                                onChange={(e) => {
-                                    let formData = this.state.formData
-                                    formData.userId = e.target.value
-                                    this.setState({formData})
-                                }}
-                                style={{width: '100%'}}
-                                validators={['required', 'isPositive']}
-                            />
-                        </Grid>
-                        <Grid item lg={6} md={6} sm={6} xm={6}>
-                            <Typography variant="body2">Id</Typography>
-                            <TextValidator
-                                id="outlined basic"
-                                placeholder="Id"
-                                variant="outlined"
-                                size="small"
-                                style={{width: '100%'}}
-                                validators={['required']}
-                                value={this.state.formData.id}
-                                onChange={(e) => {
-                                    let formData = this.state.formData
-                                    formData.id = e.target.value
-                                    this.setState({formData})
-                                }}
-                            />
-                        </Grid>
-                        <Grid item lg={6} md={6} sm={6} xm={6}>
-                            <Typography variant="body2">Title</Typography>
-                            <TextValidator
-                                id="outlined basic"
-                                placeholder="Title"
-                                variant="outlined"
-                                size="small"
-                                style={{width: '100%'}}
-                                validators={['required']}
-                                value={this.state.formData.title}
-                                onChange={(e) => {
-                                    let formData = this.state.formData
-                                    formData.title = e.target.value
-                                    this.setState({formData})
-                                }}
-                            />
-                        </Grid>
-                        <Grid item lg={6} md={6} sm={6} xm={6}>
-                            <Typography variant="body2">Body</Typography>
-                            <TextValidator
-                                id="outlined basic"
-                                placeholder="Body"
-                                variant="outlined"
-                                size="small"
-                                style={{width: '100%'}}
-                                validators={['required']}
-                                value={this.state.formData.body}
-                                onChange={(e) => {
-                                    let formData = this.state.formData
-                                    formData.body = e.target.value
-                                    this.setState({formData})
-                                }}
-                            />
-                        </Grid>
+                    <div className={classes.form_div}>
+                        <div className={classes.txt_div}>
+                            <TextValidator id="outlined-basic" label="User ID" variant="outlined" onChange={(e) => {
+                                let post = this.state.post;
+                                post.userId = e.target.value
+                            }} validators={['required', 'isPositive']}/>
+                            <TextValidator id="outlined-basic" label="Title" variant="outlined" validators={['required']} onChange={(e) => {
+                                let post = this.state.post;
+                                post.title = e.target.value
+                            }}/>
 
-                        <Grid item lg={6} md={6} sm={6} xm={6} style={{display: 'flex'}}
-                              style={{justifyContent: 'flex-end'}}>
-                            <GDSEButton variant="contained" size="small" label="save" type="submit"/>
-                        </Grid>
-                    </Grid>
+                            <TextValidator id="outlined-basic" label="ID" variant="outlined" validators={['required', 'isPositive']} onChange={(e) => {
+                                let post = this.state.post;
+                                post.id = e.target.value
+                            }}/>
+                            <TextValidator id="outlined-basic" label="Body" variant="outlined" validators={['required']} onChange={(e) => {
+                                let post = this.state.post;
+                                post.body = e.target.value
+                            }}/>
+                        </div>
+                        <div className={classes.btn_div}>
+                            <Button size="large" color={"error"} variant="outlined">Clear</Button>
+                            <Button size="large" variant="outlined" type='submit'>Save</Button>
+                        </div>
+                    </div>
                 </ValidatorForm>
-            </Fragment>
-        )
+
+                <div>
+                    <TableContainer component={Paper} sx={{maxHeight:'580px'}}>
+                        <Table sx={{ minWidth: 650 }} aria-label="simple table" stickyHeader>
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell>Customer Name</TableCell>
+                                    <TableCell>Nic</TableCell>
+                                    <TableCell>Gender</TableCell>
+                                    <TableCell>Email</TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {/*<TableRow>
+                                    <TableCell>Helitha Sri</TableCell>
+                                    <TableCell>20001256879</TableCell>
+                                    <TableCell>Male</TableCell>
+                                    <TableCell>heli@gmail.com</TableCell>
+                                </TableRow>*/}
+
+                                {this.state.customerTblData.map((row) => (
+                                    <TableRow
+                                        key={row.name}
+                                        sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                                    >
+                                        <TableCell>{row.name}</TableCell>
+                                        <TableCell>{row.nic}</TableCell>
+                                        <TableCell>{row.gen}</TableCell>
+                                        <TableCell>{row.email}</TableCell>
+                                    </TableRow>
+                                ))}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                </div>
+
+            </div>
+        </div>)
     }
+
 }
 
-export default withStyles(styleSheet)(Posts)
+export default withStyles(styleSheet)(Post);
